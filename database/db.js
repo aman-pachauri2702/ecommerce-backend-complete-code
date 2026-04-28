8fcb1f5e31f2cf3582b3429fe53fd35cc801501e
aman-pachauri2702 <pachauriaman3@gmail.com>
2026-04-28 12:19:27 +0530

    new

--- database/db.js
+++ database/db.js
@@ -1,19 +1,28 @@
+import { config } from "dotenv";
 import pkg from "pg";
-const {Client}=pkg;
-const database =new Client({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT
-,}
+const { Client } = pkg;
 
-)
+config();
+
+const database = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+
+    // ✅ ADD THIS
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 try {
     await database.connect();
     console.log("database connected successfully");
 } catch (error) {
-    console.error("database connection failed:",error);
+    console.error("database connection failed:", error);
     process.exit(1);
 }
+
 export default database;
