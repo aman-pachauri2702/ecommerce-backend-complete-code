9e1e219e1b70f3a106dc494dfce17ac90d83323b
aman-pachauri2702 <pachauriaman3@gmail.com>
2026-05-07 16:36:03 +0530

    Refactor response cookie handling in jwtToken.jsh

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,21 +5,20 @@ export const sendToken = (user, statusCode, message, res) => {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-  res
-    .status(statusCode)
-   .cookie("token", token, {
-  expires: new Date(
-    Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-  ),
-  httpOnly: true,
-  secure: true,     // ✅ localhost ke liye
-  sameSite: "None",   // ✅ cookie send hogi
-})
-
-    .json({
-      success: true,
-      user,
-      message,
-      token,
-    });
-};
+ res
+  .status(statusCode)
+  .cookie("token", token, {
+    expires: new Date(
+      Date.now() +
+        process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  })
+  .json({
+    success: true,
+    user,
+    message,
+    token,
+  });
